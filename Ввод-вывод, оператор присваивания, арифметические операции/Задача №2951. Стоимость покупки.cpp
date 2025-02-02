462b4d0fd5574f20e41b8162978dc8cfc9628dc4
moera112 <andrlavr16@gmail.com>
2025-02-02 19:19:23 +0300

    Create  Задача №2951. Стоимость покупки.cpp

---
+++ Ввод-вывод, оператор присваивания, арифметические операции/Задача №2951. Стоимость покупки.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, n;
+
+    cin >> a;
+    cin >> b;
+    cin >> n;
+
+    a *= n;
+    b *= n;
+
+    int c = b % 100;
+    a += b / 100;
+
+    cout << a << " " << c << endl;
+
+    return 0;
+}
