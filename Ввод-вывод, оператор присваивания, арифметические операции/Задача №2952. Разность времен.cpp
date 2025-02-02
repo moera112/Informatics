be63eaf360c7de22712037f589c19a43cade932e
moera112 <andrlavr16@gmail.com>
2025-02-02 19:21:22 +0300

    Create  Задача №2952. Разность времен.cpp

---
+++ Ввод-вывод, оператор присваивания, арифметические операции/Задача №2952. Разность времен.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d, e, f;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+
+    int total_seconds = 3600 * (d - a) + 60 * (e - b) + (f - c);
+    cout << total_seconds << endl;
+
+    return 0;
+}
